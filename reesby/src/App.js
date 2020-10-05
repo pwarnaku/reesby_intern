992ce397d60350e9d6cb5fc92f032f925d10a53b
Piumi Fernando <pwarnaku@deakin.edu.au>
2020-10-05 21:44:18 +1100

    added promises

--- reesby/src/App.js
+++ reesby/src/App.js
@@ -1,34 +1,52 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
+import Datatable from "./datatable/index";
 import Button from '@material-ui/core/Button';
 import Search from './components/Search';
 
-import './App.css';
+require("es6-promise").polyfill();
+require("isomorphic-fetch");
 
 function App() {
+
+  const [data, setData] = useState([]);
+  const [q, setQ] = useState("");
+
+  useEffect(() => {
+ 
+    fetch("http://javareesbyapi-env.eba-rtdeyeqd.ap-southeast-2.elasticbeanstalk.com/api/v1/getallclients/tenant/reesby?api_key=b7c58b")
+    .then ((response )=> response.json())
+   .then((json) => setData(json));
+
+  
+  }, 
+  
+  []);
+
   return (
+
     <div>
-    <div className="top">
-    <h2 >Clients</h2>
-    <div>
-    <Button variant="outlined">+ NEW CLIENT</Button>
- 
-    </div>
-     </div>
-    
-    
-     <div className="head">
-     <div > <Search/></div>
-    
-     <div className="btns"><Button variant="outlined">Search</Button></div>
-     <div className="btns"><Button variant="outlined">Clear</Button></div>
-  
-      
-     </div>
+      <div className="top">
+      <h2 >Clients</h2>
+      <div>
+      <Button variant="outlined">+ NEW CLIENT</Button>
    
+      </div>
+       </div>
+      
+      
+       <div className="search-btns">
+       <div > <Search/></div>
+      
+       <div className="btns"><Button variant="outlined">Search</Button></div>
+       <div className="btns"><Button variant="outlined">Clear</Button></div>
     
-  
-  </div>
- 
+        
+       </div>
+     
+       <Datatable data ={data}/>
+    
+    </div>
+   
     
   );
 }
---
+++ reesby/src/datatable/index.js
@@ -0,0 +1,61 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+
+
+export default function Datatable({data}) {
+
+const useStyles = makeStyles({
+    table: {
+      minWidth: 650,
+    },
+  });
+ const classes = useStyles();
+
+ /*tested codes
+//var jsonArray = JSON.parse(JSON.stringify(data));
+//const columns = data[0] && Object.keys(data[0]);
+//console.log(tmpArray);
+*/
+  return (
+      <TableContainer component={Paper} style={{border: "1px solid rgb(0, 0, 0)"}}>
+      <Table className={classes.table} aria-label="simple table">
+        <TableHead>
+
+       
+        <TableRow style={{border: "2px solid rgb(0, 0, 0)"}}>
+        <TableCell>Client Name</TableCell>
+            <TableCell>Email</TableCell>
+            <TableCell>Phone</TableCell>
+            <TableCell>Industry</TableCell>
+            <TableCell>Point of Contract</TableCell>
+            <TableCell>Website</TableCell>
+       </TableRow>
+        </TableHead>
+        <TableBody>
+
+        {data.map(el => {
+            return (
+              <TableRow key={el.clientId}>
+                <TableCell>{el.clientName}</TableCell>
+                <TableCell>{el.clientEmail}</TableCell>
+                <TableCell>{el.clientWorkPhone}</TableCell>
+                <TableCell>{el.clientIndustry}</TableCell>
+                <TableCell>{el.clientContract}</TableCell>
+                <TableCell>{el.clientWebsite}</TableCell>
+              </TableRow>
+            );
+          })}
+
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
+}
+
